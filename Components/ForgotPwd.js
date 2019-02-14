2e75d0aafce2dd14da40b2a7227a2148cb07c494
BeFunes <benedetta.dalcanton@gmail.com>
2019-02-14 13:31:51 +0100

    forgot psw & gitignore

--- Components/ForgotPwd.js
+++ Components/ForgotPwd.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import {SafeAreaView, View, StyleSheet, TextInput, ActivityIndicator, Button, Text, Alert} from 'react-native'
+import {
+	SafeAreaView, View, StyleSheet, TextInput, ActivityIndicator, Button, Text, Alert,
+	TouchableOpacity
+} from 'react-native'
 import firebase from 'react-native-firebase';
 import {formatEmail} from "./../utils/validation";
 
@@ -28,11 +31,20 @@ class ForgotPwd extends React.Component {
 		this.setState({is_load: true})
 		firebase.auth().sendPasswordResetEmail(this.state.user_email.trim())
 			.then(() => {
-				this.props.navigation.navigate('Login')
+				this.setState({is_load: false})
+				Alert.alert("Check your emails!", "You received a link to reset your password",
+					[
+						{
+							text: "OK", onPress: async () => {
+								this.props.navigation.navigate('Login')
+							}
+						}
+					],
+					{cancelable: false})
 				console.log("sucess")
 			})
 			.catch((err) => {
-				this.setState({is_load: false, error: err.message})
+				this.setState({is_load: false, error: "Sorry, we couldn't find a user linked ot this email address"})
 				console.log(err)
 			})
 
@@ -44,21 +56,38 @@ class ForgotPwd extends React.Component {
 		return (
 			<SafeAreaView style={styles.main_container}>
 				<Text style={styles.titre}>Connexion</Text>
-				<TextInput
-					placeholder="Email"
-					style={styles.textInput} onChangeText={(email) => {
-					this.setState({user_email: email, error: false})
-				}}/>
-				<Button
-					title='Envoyer'
-					disabled={!showButton}
-					onPress={() => this._forgotPWD()}
-				/>
-				{this.state.error && <View style={{alignItems: 'center', paddingLeft: 10, paddingRight: 10}}>
-					<Text style={styles.errorText}>
-						{this.state.error}
-					</Text>
-				</View>}
+				<View>
+					<TextInput
+						placeholder="Email"
+						style={styles.textInput} onChangeText={(email) => {
+						this.setState({user_email: email, error: false})
+					}}/>
+					<Button
+						title='Envoyer'
+						disabled={!showButton}
+						onPress={() => this._forgotPWD()}
+					/>
+					{this.state.error && <View style={{alignItems: 'center', paddingLeft: 10, paddingRight: 10}}>
+						<Text style={styles.errorText}>
+							{this.state.error}
+						</Text>
+					</View>}
+				</View>
+
+				<View style={{alignItems: 'center', justifyContent: 'flex-end', flexGrow: 1}}>
+					<TouchableOpacity
+						style={styles.button}
+						onPress={() => this.props.navigation.navigate('Signup')}
+						underlayColor='#fff'>
+						<Text style={styles.text_btn}>Sign up</Text>
+					</TouchableOpacity>
+					<TouchableOpacity
+						style={styles.button}
+						onPress={() => this.props.navigation.navigate('Login')}
+						underlayColor='#fff'>
+						<Text style={styles.text_btn}>Log in</Text>
+					</TouchableOpacity>
+				</View>
 				{this._displayLoading()}
 			</SafeAreaView>
 		)
@@ -68,6 +97,7 @@ class ForgotPwd extends React.Component {
 const styles = StyleSheet.create({
 	main_container: {
 		flex: 1,
+		// justifyContent: 'space-between',
 		backgroundColor: '#191414',
 	},
 	titre: {
@@ -83,8 +113,23 @@ const styles = StyleSheet.create({
 		color: 'white'
 	},
 	errorText: {
-		color: 'red'
-	}
+		color: 'red',
+		margin: 20,
+	},
+	button: {
+		height: 50,
+		width: 300,
+		marginTop: 20,
+		backgroundColor: '#191414',
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius: 5,
+	},
+	text_btn: {
+		color: "rgb(55,128,243)",
+		textAlign: 'center',
+		fontSize: 18
+	},
 })
 
 
