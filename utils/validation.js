2a12038432dddf379d85dfd1f55a6993ec42f8c7
BeFunes <benedetta.dalcanton@gmail.com>
2019-03-14 12:23:01 +0100

    email validation correction

--- utils/validation.js
+++ utils/validation.js
@@ -1,4 +1,4 @@
-export const formatEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+export const formatEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/
 
 export const formatPwd = /^.{6,}$/
 
